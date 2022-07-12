cf48fd492565d1a936c19f786afa1a72d846e36b
Mounisha.M <Mounisha.M@SEZLP657.avasoft.com>
2022-07-12 20:33:03 +0530

    First Commit

---
+++ Test/src/main/java/Pac1/Ashottest.java
@@ -0,0 +1,31 @@
+package Pac1;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+public class Ashottest {
+	public static void main(String[] args) throws IOException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver  = new ChromeDriver();	
+
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		driver.manage().window().maximize();
+		
+		ru.yandex.qatools.ashot.Screenshot s = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000))
+				.takeScreenshot(driver);
+				
+
+		ImageIO.write(s.getImage(), "jpg", new File("D:\\ElementScreenshot.jpg"));
+		
+	}
+}
